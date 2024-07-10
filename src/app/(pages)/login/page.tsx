f15aed82e1d3f31a9a385a3d3d0e36df1c49d6b5
hemang-mehta <hemangmehta1703@gmail.com>
2024-07-10 17:31:24 +0530

    first commit

---
+++ src/app/(pages)/login/page.tsx
@@ -0,0 +1,10 @@
+export default function Login(){
+    return (
+        <main>
+            <title>
+                Login
+            </title>
+            <h1>hey this is loginnnnnnn!!!</h1>
+        </main>
+    )
+}
